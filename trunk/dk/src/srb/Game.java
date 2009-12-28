95e637ced6f7cde1f723cd4da42a045ab240ed00
srbharadwaj <srbharadwaj@b93d94f8-f3a7-11de-b359-dd99991af1b2>
2009-12-28 15:41:07 +0000

    First Check-In

---
+++ trunk/dk/src/srb/Game.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package srb;
+
+import java.util.Vector;
+
+/**
+ * Class Name - Game
+ * Description - 
+ *
+ * @author suhas
+ */
+public class Game
+{
+    public Vector moveList = new Vector();
+    public CB cbGame = null;
+    public int gameMoveNo = 0;
+    public String gameResult = "*";
+    public int gameno=0;
+    public String wPlayer = "Suhas";
+    public String bPlayer = "Player"+gameno;
+    public Vector tags = new Vector();
+
+    public Game(CB cb,int i)
+    {
+        cbGame = cb;
+        gameno=i;
+        wPlayer = "Suhas";
+        bPlayer = "Player"+gameno;
+    }
+
+    public void addMove(int mn,String s)
+    {
+        //a new move is done after clicking prev button and taking back a move
+        //hence remove the rest of the moves which was there previously
+        while(true)
+        {
+            try {
+            moveList.removeElementAt(mn-1);
+            } catch(ArrayIndexOutOfBoundsException e) {
+                break;
+            }
+        }
+        System.out.println("MM "+s);
+        Move m = new Move(mn,s,cbGame);
+        moveList.add(m);
+        gameMoveNo++;
+    }
+
+    public void getMove(int mn)
+    {
+        System.out.println("mn"+mn);
+        Move m = (Move) moveList.get(mn-1);
+        m.getMoveData(cbGame);
+    }
+
+    public void setGameResult(String res)
+    {
+        gameResult = res;
+        Move m = (Move) moveList.get(moveList.size()-1);
+        if(m.moveNo%2==0)
+        {
+            m.movePGNString = m.movePGNString+"\n"+res;
+        }
+        else
+        {
+            m.movePGNString = m.movePGNString+" "+res;
+        }
+    }
+
+    public void updateGame()
+    {
+        if(tags.size()>0)
+        {
+            for(int i=0;i<tags.size();i++)
+            {
+                String s = (String) tags.get(i);
+                if(s.contains("White"))
+                {
+                    wPlayer = s.split(":")[1];
+                }
+                else if(s.contains("Black"))
+                {
+                    bPlayer = s.split(":")[1];
+                }
+                else if(s.contains("Result"))
+                {
+                    gameResult = s.split(":")[1];
+                }
+            }
+        }
+    }
+
+}
---
+++ trunk/dk/src/srb/PGNFileFilter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package srb;
+
+/**
+ * Class Name - PGNFileFilter
+ * Description - 
+ *
+ * @author suhas
+ */
+    class PGNFileFilter extends javax.swing.filechooser.FileFilter
+    {
+        public String getDescription() {
+            return "PGN Files (*.pgn)";
+        }
+        public boolean accept(java.io.File file) {
+            return file.exists() && file.canRead() && (file.getName().endsWith(".pgn") || file.isDirectory());
+        }
+    }
